1be130e611292e3d2dc7076771520ebe125b7bbe
kevincao1119 <43764834+kevincao1119@users.noreply.github.com>
2018-11-12 15:05:20 -0500

    Update Temp.c

--- Temp.c
+++ Temp.c
@@ -13,61 +13,62 @@
 #define low "lowest temperature is "
 #define high "highest temperature is "
 #define nows "current temperature is "
-double gettemp(int fd, char *buff);
+
 
 int main(int argc, char *argv[])
 {
- FILE *fd;
- int n, first, last;
- double y, high, low;
- char buf[500];
- char *temp;
- char l[100], cur[100],h[100];
- time_t start = time(NULL);
- int next = start+1;
- first =0;
+ FILE *fd;      /*file pointer*/
+ int n,first,last; /*y is current, first is boolean,last is last t*/
+ double y, low,high;
+char buf[500];
+ char *temp;    /* temp pointer*/
+ char l[100], cur[100],h[100];    /*different temp message store space*/
+ time_t start = time(NULL);  /*current time*/
+ int next = start+1;           /* iterator in loop*/
+ first =0;                       /* boolean false*/
  while(start < next){
- if((fd = fopen("/sys/bus/w1/devices/28-0023984300c5/w1_slave", "r")) == (FILE *)NULL) {
+ if((fd = fopen("/sys/bus/w1/devices/28-0213133656aa/w1_slave", "r")) == (FILE *)NULL) {
  perror("Error: Failed to Open w1_slave file");
  (void) exit(1);
  }
- n = fread(buf, 1, 100, fd);//eads data from the given stream fd into the array pointed to, by ptr--buf
+ n = fread(buf, 1, 100, fd);
  if(n == 0) {
  perror("Error: No Arguments Found");
  exit(1);
  }
- buf[n] = '\0';
- fprintf(stdout, "Read %s\n",buf+69);//sends formatted output to a stream,printing output on the main output device for the session
- (void) fclose(fd);//closes the stream. All buffers are flushed.
- temp = buf+69;
+ buf[n] = '\0';          
+ fprintf(stdout, "Read '%s'\n",buf+69);   /*location after 69char is temp show in screen*/
+ (void) fclose(fd);
+ temp = buf+69;/*temp print after 29750, then start print 2 after +69*/ 
  printf("Temp: %s",temp);
- y = atof(temp); //converts the string argument str to a floating-point number
+ y = atof(temp);       /*change from char to int*/
  y=y/1000;
  printf("Integer Y: %.1f\n",y);
- if(first == 0){
+ if(first == 0){  
 	low = y;
 	high = y;
 	first =1;
 	last = y;
-	sprintf(l,"Lowest Temp: %.1f C",low);
-	sprintf(cur,"Current Temp: %.1f C",y);
-	sprintf(h,"Highest Temp: %.1f C",high);
-	ifttt("https://maker.ifttt.com/trigger/alarm_triggered/with/key/dhiJR-IZGw4iy-XaJAqJSfy8ArgkPMFFGVC94n0Wrl4", l,cur, h);
+	sprintf(l,"Lowest Temp: %d C",low/1000);
+	sprintf(cur,"Current Temp: %d C",y/1000);
+	sprintf(h,"Highest Temp: %d C",high/1000);
+	ifttt("https://maker.ifttt.com/trigger/112/with/key/bxK2Vz1Sd9_Qiq2mF8RqrQ", l,cur, h);
 	}
  else{
 	if(y <low){low = y;}
-	if(y>high){high = y;}
-	if(((last-y)>=1) ||((y-last)>=1)){
-	sprintf(l,"Lowest Temp: %.1f C",low);
-	sprintf(cur,"Current Temp: %.1f C",y);
-	sprintf(h,"Highest Temp: %.1f C",high);
-	ifttt("https://maker.ifttt.com/trigger/alarm_triggered/with/key/dhiJR-IZGw4iy-XaJAqJSfy8ArgkPMFFGVC94n0Wrl4", l,cur, h);
-	last = y;
+	else if(y>high){high = y;}
+	else if(((last-low)>=1000) ||((high-last)>=1000)){
+	sprintf(l,"Lowest Temp: %d C",low/1000);
+        sprintf(cur,"Current Temp: %d C",y/1000);
+        sprintf(h,"Highest Temp: %d C",high/1000);
+	  ifttt("https://maker.ifttt.com/trigger/112/with/key/bxK2Vz1Sd9_Qiq2mF8RqrQ", l,cur, h);
+	  last = y;
 	}
+	
 	}
  printf("High: %.1f Current: %.1f Low: %.1f\n",high,y,low);
  start = time(NULL);
- next = start+1;
+ next = start+1;                    /*reassign the next to make loop continue*/
 }
  return 0;
 }
